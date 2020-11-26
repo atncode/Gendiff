acb9efd1cd3ef122ef95d39464597afadd7ab42f
atncode <billowcode@gmail.com>
2020-11-26 16:39:07 +0700

    add option format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,8 +6,11 @@ const program = new Command();
 
 program
   .version('0.0.1')
-  .description('Usage: gendiff [options]')
+  .description('Usage: gendiff [options] <filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
+
 
 program.parse(process.argv);
 
