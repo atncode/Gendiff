3b7e1a3a73c24215dfb0109567f6e82ef19a534f
atncode <billowcode@gmail.com>
2020-12-04 15:12:50 +0700

    fix errors

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,5 +1,4 @@
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 import path from 'path';
 import genDiff from '../src/index.js';
 import parse from '../src/parser.js';
@@ -7,9 +6,8 @@ import expected from '../__fixtures__/expected_file.js';
 
 test('compare JSON files', () => {
   const __filename = fileURLToPath(import.meta.url);
-  const __dirname = dirname(__filename);
+  const __dirname = path.dirname(__filename);
   const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
   const actual = genDiff(parse(getFixturePath('file1.json')), parse(getFixturePath('file2.json')));
   expect(actual).toEqual(expected);
-})
-
+});
