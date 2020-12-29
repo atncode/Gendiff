9c4652ea6c2804d13e778e1dd5d9ba3bbe55fe2f
atncode <billowcode@gmail.com>
2020-12-29 19:37:35 +0700

    fix diff function

--- src/diff.js
+++ src/diff.js
@@ -3,41 +3,32 @@ import _ from 'lodash';
 const diff = (obj1, obj2) => {
   const keys1 = _.keys(obj1);
   const keys2 = _.keys(obj2);
-  const keys = _.union(keys1, keys2).sort();
+  const keys = _.union(keys1, keys2);
 
-  return keys.flatMap((key) => {
+  return _.sortBy(keys).flatMap((key) => {
     const value1 = obj1[key];
     const value2 = obj2[key];
-    let leaf;
 
-    switch (true) {
-      case (value1 instanceof Object) && (value2 instanceof Object):
-        leaf = { key, children: diff(value1, value2), state: 'nested' };
-        break;
-
-      case !_.has(obj1, key):
-        leaf = { key, value: value2, state: 'added' };
-        break;
-
-      case !_.has(obj2, key):
-        leaf = { key, value: value1, state: 'removed' };
-        break;
-
-      case value1 === value2:
-        leaf = { key, value: value1, state: 'unchanged' };
-        break;
-
-      case value1 !== value2:
-        leaf = {
-          key, value1, value2, state: 'updated',
-        };
-        break;
-
-      default:
-        console.log('This state does not exists');
-        break;
+    if (!_.has(obj1, key)) {
+      return { key, value: value2, state: 'added' };
     }
-    return leaf;
+    if (!_.has(obj2, key)) {
+      return { key, value: value1, state: 'removed' };
+    }
+    if (_.isObject(value1) && _.isObject(value2)) {
+      return { key, children: diff(value1, value2), state: 'nested' };
+    }
+    if (value1 !== value2) {
+      return {
+        key, value1, value2, state: 'updated',
+      };
+    }
+    if (value1 === value2) {
+      return {
+        key, value: value1, state: 'unchanged',
+      };
+    }
+    return 'unknown case';
   });
 };
 
