fef1ffa4777d43a301580332fbe826766a3c0c86
atncode <billowcode@gmail.com>
2020-12-30 18:36:05 +0700

    fix stylish

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,53 +1,57 @@
 import _ from 'lodash';
 
-const getCurrentIndent = (depth, state, replacer = ' ', spacesCount = 4) => {
-  const states = ['added', 'removed', 'updated'];
-  const indentSize = states.includes(state) ? ((depth * spacesCount) - 2) : (depth * spacesCount);
-  return replacer.repeat(indentSize);
+const replacer = ' ';
+const spacesCount = 4;
+const correctiveIndent = 2;
+
+const stringify = (value, currentDepth) => {
+  const iter = (currentValue, depth) => {
+    if (!_.isObject(currentValue)) {
+      return currentValue;
+    }
+    const indentSize = depth * spacesCount;
+    const currentIndent = replacer.repeat(indentSize);
+    const bracketIndent = replacer.repeat(depth * spacesCount - spacesCount);
+    const lines = Object
+      .entries(currentValue)
+      .map(([key, val]) => `${currentIndent}${key}: ${iter(val, depth + 1)}`);
+
+    return [
+      '{',
+      ...lines,
+      `${bracketIndent}}`,
+    ].join('\n');
+  };
+
+  return iter(value, currentDepth);
 };
-
-const getBracketIndent = (depth, replacer = ' ', spacesCount = 4) => replacer.repeat(depth * spacesCount - spacesCount);
-
-const getFormattedValue = (value, depth) => {
-  const { state } = value;
-  const entries = _.entries(value);
-
-  const result = entries.flatMap(([currentKey, currentValue]) => {
-    const val = _.isObject(currentValue)
-      ? getFormattedValue(currentValue, depth + 1)
-      : currentValue;
-    return `${getCurrentIndent(depth, state)}${currentKey}: ${val}`;
-  });
-  return ['{', ...result, `${getBracketIndent(depth)}}`].join('\n');
-};
-
-const getValue = (value, depth) => (_.isObject(value) ? getFormattedValue(value, depth) : value);
 
 const formatter = (ast) => {
   const iter = (currentValue, depth) => {
+    const indentSize = (depth * spacesCount) - correctiveIndent;
+    const currentIndent = replacer.repeat(indentSize);
+    const bracketIndent = replacer.repeat(depth * spacesCount - spacesCount);
+
     const result = currentValue.flatMap((obj) => {
       const {
         key, value, value1, value2, state, children,
       } = obj;
-      const val = getValue(value, depth + 1);
-      const val1 = getValue(value1, depth + 1);
-      const val2 = getValue(value2, depth + 1);
       switch (state) {
         case 'added':
-          return `${getCurrentIndent(depth, state)}+ ${key}: ${val}`;
+          return `${currentIndent}+ ${key}: ${stringify(value, depth + 1)}`;
         case 'removed':
-          return `${getCurrentIndent(depth, state)}- ${key}: ${val}`;
+          return `${currentIndent}- ${key}: ${stringify(value, depth + 1)}`;
         case 'unchanged':
-          return `${getCurrentIndent(depth, state)}${key}: ${val}`;
+          return `${currentIndent}  ${key}: ${stringify(value, depth + 1)}`;
         case 'updated':
-          return `${getCurrentIndent(depth, state)}- ${key}: ${val1}\n${getCurrentIndent(depth, state)}+ ${key}: ${val2}`;
+          return `${currentIndent}- ${key}: ${stringify(value1, depth + 1)}\n${currentIndent}+ ${key}: ${stringify(value2, depth + 1)}`;
         case 'nested':
-          return `${getCurrentIndent(depth, state)}${key}: ${iter(children, depth + 1)}`;
+          return `${currentIndent}  ${key}: ${iter(children, depth + 1)}`;
         default:
           throw new Error(`Unknown state ${state}!`);
       }
     });
-    return ['{', ...result, `${getBracketIndent(depth)}}`].join('\n');
+    return ['{', ...result, `${bracketIndent}}`].join('\n');
   };
 
   return iter(ast, 1);
